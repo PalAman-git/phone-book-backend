39a88251db4eae5375209fb680353753d527a922
Aman <Palaman2626@gmail.com>
2023-08-07 17:04:39 +0530

    added eslint

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
 require('dotenv').config();
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const Person = require('./model/person');
 
 //middleware
 app.use(express.json());
-morgan.token("body", (req) => {
-  return JSON.stringify(req.body);
+morgan.token('body', (req) => {
+	return JSON.stringify(req.body);
 });
 
 // app.use(morgan('tiny'));//middleware
-app.use(morgan(":method :url :status :res[content-length] :req[header] :response-time :body")); //middleware
+app.use(morgan(':method :url :status :res[content-length] :req[header] :response-time :body')); //middleware
 
 
-app.get("/api/persons", (req, res) => {//working
-  Person.find({}).then( persons => res.json(persons));
+app.get('/api/persons', (req, res) => {//working
+	Person.find({}).then( persons => res.json(persons));
 });
 
-app.get("/info", (req, res) => {//working
-  const length = Person.length
-  res.send(`Phonebook has info of ${length} people <br/> ${new Date()}`);
+app.get('/info', (req, res) => {//working
+	const length = Person.length;
+	res.send(`Phonebook has info of ${length} people <br/> ${new Date()}`);
   
 });
 
-app.get("/", (req, res) => {//working
-  res.send(
-    "You are on the homepage of the phone directory got to /api/persons for numbers and information"
-  );
+app.get('/', (req, res) => {//working
+	res.send(
+		'You are on the homepage of the phone directory got to /api/persons for numbers and information'
+	);
 });
 
-app.get("/api/persons/:id", (req, res) => {//working
-  Person.findById(req.params.id).then((person) => res.json(person));
+app.get('/api/persons/:id', (req, res) => {//working
+	Person.findById(req.params.id).then((person) => res.json(person));
 });
 
-app.delete("/api/persons/:id", (req, res, next) => {//working
-  Person.findOneAndRemove(req.params.id)
-    .then(() => res.status(200).end())
-    .catch((err) => next(err));
+app.delete('/api/persons/:id', (req, res, next) => {//working
+	Person.findOneAndRemove(req.params.id)
+		.then(() => res.status(200).end())
+		.catch((err) => next(err));
 });
 
-app.post("/api/persons", (req, res) => {//working
-  const body = req.body;
+app.post('/api/persons', (req, res) => {//working
+	const body = req.body;
 
-  if (body === undefined || !body || !body.name || !body.number)
-    return res.status(400).json({ error: "content missing" });
+	if (body === undefined || !body || !body.name || !body.number)
+		return res.status(400).json({ error: 'content missing' });
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  });
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	});
 
-  person.save().then((person) => {
-    res.json(person);
-  });
+	person.save().then((person) => {
+		res.json(person);
+	});
 });
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error);
+	console.log(error);
 
-  if (error.name === "CastError") {
-    return res.status(400).json({ error: "malformatted id" });
-  }
+	if (error.name === 'CastError') {
+		return response.status(400).json({ error: 'malformatted id' });
+	}
 
-  next(error);
+	next(error);
 };
 app.use(errorHandler);//it has to be the last middleware
 
@@ -72,5 +72,5 @@ app.use(errorHandler);//it has to be the last middleware
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`app listening at port ${PORT}`);
+	console.log(`app listening at port ${PORT}`);
 });
--- model/person.js
+++ model/person.js
@@ -1,34 +1,47 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI;
 
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 
 mongoose.connect(url)
 
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+	.then(() => {
+		console.log('connected to MongoDB');
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message);
+	});
 
 const noteSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
-})
+	name: {
+		type:String,
+		minLength:3,
+	},
+	number: {
+		type: String,
+		validate: {
+			validator: function(value) {
+				return /^[0-9]{2,3}-[0-9]+$/.test(value);
+			},
+			message: props => `${props.value} is not a valid format. It should be XX-XXX or XXX-XXXX, where X is a number.`,
+		},
+		required: true,
+	},
+});
 
 noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	}
+});
 
 
 module.exports = mongoose.model('Person', noteSchema);
